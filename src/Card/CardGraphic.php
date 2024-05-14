97b85ca678a84dfd2b38949a1d96b071dc94877a
claes k <ckck.tumblr@gmail.com>
2024-05-14 14:35:56 +0200

    Clean up CardGraphic.php

--- src/Card/CardGraphic.php
+++ src/Card/CardGraphic.php
@@ -5,25 +5,6 @@ namespace App\Card;
 class CardGraphic extends Card
 {
     protected $graphic;
-    // private $spades = [
-    //     'https://upload.wikimedia.org/wikipedia/commons/5/5a/Ace_of_spades.svg',
-    //     '♠2',
-    //     '♠3',
-    //     '♠4',
-    //     '♠5',
-    //     '♠6',
-    //     '♠7',
-    //     '♠8',
-    //     'https://upload.wikimedia.org/wikipedia/commons/6/63/9_of_spades.svg',
-    //     '♠10',
-    //     'https://upload.wikimedia.org/wikipedia/commons/a/a9/Jack_of_spades2.svg',
-    //     '♠Q',
-    //     '♠K',
-    // ];
-
-    // ♥
-    // ♦
-    // ♣
 
     public function __construct($suitInput, $valueInput, $graphicInput)
     {
@@ -34,10 +15,6 @@ class CardGraphic extends Card
 
     public function getAsString(): string
     {
-        // if ($this->suit == "1" || $this->suit == "♠" || $this->suit == "Spades") {
-        //     return $this->spades[$this->value - 1];
-        //     return "{$this->value} of {$this->suit}";
-        // }
         return $this->graphic;
     }
 }
